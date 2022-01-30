a6013c1be94f1ff20b23ccd49cadee84b04d3bdc
clarestyle2022 <clarestyle@live.com>
2022-01-30 09:50:13 +0000

    New testng class TC004

---
+++ jenkins/src/test/java/testcases/TC004.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC004 {
+  @Test
+  public void f() throws Exception {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\clare\\OneDrive\\Desktop\\Selenium\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  Thread.sleep(5000);
+	//  driver.manage().window().maximize();
+	  driver.get("https://www.newtours.us/");
+	  Thread.sleep(2000);
+	  driver.close();
+	  driver.quit();
+	  
+  }
+}
